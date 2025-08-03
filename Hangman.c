6e91c67975cc94c49df2e404fa669c2ea110c21d
Jack Wiegman <33030441+jackwiegman@users.noreply.github.com>
2025-08-03 17:58:40 -0600

    Add basic functionality

--- Hangman.c
+++ Hangman.c
@@ -3,17 +3,20 @@
  * @brief Play a hangman game with the user.
  */
 
+#include <endian.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define CHAR_SIZE sizeof(char)      /** Size of char on the computer running the program */
 #define INT_SIZE sizeof(int)        /** Size of an int on the computer running the program */
 #define MAX_HANGMAN_STRING_SIZE 100 /** Max string size to be entered by the player */
 #define MAX_ERRORS 7                /** Max error before player loses */
-#define MAX_GUESSED_LETTERS         /** Max size of array to hold the guessed letters */
+#define MAX_GUESSED_LETTERS 26      /** Max size of array to hold the guessed letters */
 
+const char *MASKED_CHAR_VALUE = "_";
 /**
  * @var const char * HANGMANPICS
  * Array holding pictures of a hangman at various stages to display.
@@ -74,11 +77,11 @@ const char *HANGMANPICS[(MAX_ERRORS)] = {"\
  * List of possible words to choose from, separated by spaces.
  */
 const char *FullWordList =
-    "ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey "
-    "duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule "
-    "newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark "
-    "sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale "
-    "wolf wombat zebra ";
+    "ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey \
+    duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule \
+    newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark \
+    sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale \
+    wolf wombat zebra ";
 
 /**
  * @var const char * usedLetters
@@ -92,18 +95,114 @@ const char *usedLetters;
  * @param[out] guessedLetter The pointer to place the guessed letter.
  */
 void getLetterGuess(char *guessedLetter) {
-    printf("Please enter a letter to guess: ");
-    while (!(scanf("%c", guessedLetter))) {
-        puts("Error, please enter a singular letter as a guess.");
-    }
+
+    printf("Please enter a letter to guess: "); // Prompt for guess
+    scanf("%s", guessedLetter);
+
+    /* Scan and reprompt untill valid input */
+    // while (scanErr != 1) {
+    //     puts("Error, please enter a singular letter as a guess.");
+    //     printf("Guess: ");
+    //     scanf("%c", guessedLetter);
+    // }
 }
 
 void runGame() {}
 
-void printCurrentBoard(char *currentBoard, int size) {
+void printCurrentBoard(char *currentBoard) {
+    int size = strlen(currentBoard) / sizeof(char *);
     for (int i = 0; i < size; i++) {
         printf("%s", &currentBoard[i]);
     }
+    printf("\n");
+}
+
+// char *getSecretWord(const char *wordList) {
+//     // Must be freed later
+//     char *modifiedList = strdup(wordList);
+//
+//     char *delimiter = " "; //
+//
+//     int wordCount = 0;
+//     unsigned long size = 0;
+//
+//     /* ----- Tokenizing and getting word count, then going thru and adding to array ---- */
+//     char *token = strtok(modifiedList, delimiter); // First token
+//     while (token != NULL) {
+//         wordCount++;                     // Keep track of number of words in list
+//         size += sizeof(strlen(token));   // Track size in bytes of list (with null byte)
+//         token = strtok(NULL, delimiter); // Move to next word in list
+//     }
+//
+//     printf("Size: %ld\n", size);
+//     printf("Word Count: %d\n", wordCount);
+//
+//     char *tokenizedWordsArray[wordCount]; // Array to store individual words
+//
+//     // for (int i = 0; i < wordCount; i++) {
+//     //     printf("%s, ", tokenizedWordsArray[i]);
+//     // }
+//     //
+//     token = strtok(modifiedList, " "); // Move to first
+//
+//     for (int i = 0; i < wordCount; i++) {
+//         puts("out 1");
+//         printf("%s\n", token);
+//         puts("out 2");
+//         if (token == NULL) {
+//             printf("%d\n", i);
+//             perror("Error when parsing word list.");
+//             break;
+//         }
+//         tokenizedWordsArray[i] = malloc(strlen(token));
+//         puts("out 3");
+//         strcpy(tokenizedWordsArray[i], token);
+//     }
+//
+//     int randomIndex = rand() % wordCount;
+//
+//     return tokenizedWordsArray[randomIndex];
+// }
+
+void printHangmanPictures() {
+    for (int i = 0; i < MAX_ERRORS; i++) {
+        printf("%s\n", HANGMANPICS[i]);
+    }
+}
+
+/**
+ * @brief Check the users guess, updating the board and tracking if the game was won.
+ *
+ * @param[in] guessedLetter The letter guessed by the user
+ * @param[in] secretWord The secret word being guessed
+ * @param[in|out] currentBoard Current state of the board
+ * @param[in] size Number of letters in the secret word & current board
+ * @return Value representing weather the game was won during this guess.
+ */
+bool checkGuess(char guessedLetter, char *secretWord, char *currentBoard, int size, bool gameWon) {
+    gameWon = true;
+    int discoveredLetters = 0;
+    puts("Exit 9");
+    for (int i = 0; i < size; i++) {
+        if (secretWord[i] == guessedLetter) {
+            currentBoard[i] = guessedLetter;
+            discoveredLetters++;
+        }
+        puts("Exit 10");
+        if (strcmp(&currentBoard[i], "_")) {
+            gameWon = false;
+        }
+    }
+
+    bool guessIsCorrect;
+    if (discoveredLetters > 0) {
+        guessIsCorrect = true;
+        printf("You discovered %d letters!", discoveredLetters);
+    } else {
+        guessIsCorrect = false;
+        printf("The letter '%c' is not in the secret word.", guessedLetter);
+    }
+    return guessIsCorrect;
 }
 
 /**
@@ -115,19 +214,58 @@ void printCurrentBoard(char *currentBoard, int size) {
  */
 int main(int argc, char *argv[]) {
 
-    char *secretWord, *currentBoard;
+    // srand(time(NULL)); // Initialize for rand()
+    // //
+    // puts(getSecretWord(FullWordList));
+    //
 
-    int size = (sizeof(&currentBoard)) / CHAR_SIZE;
+    printHangmanPictures();
 
-    for (int i = 0; i < MAX_ERRORS; i++) {
-        printf("%s\n", HANGMANPICS[i]);
+    char *secretWord = "apricot";
+    puts("exit 1");
+
+    int secretWordSize = strlen(secretWord);
+    printf("sec word size: %d\n", secretWordSize);
+    puts("exit 2");
+
+    char *currentBoard[secretWordSize];
+    puts("exit 3");
+
+    char playerGuess;
+
+    puts("exit 2");
+    for (int i = 0; i < secretWordSize; i++) {
+        currentBoard[i] = strdup(MASKED_CHAR_VALUE);
+        // strcpy(currentBoard[i], MASKED_CHAR_VALUE);
     }
 
-    // wordList[numWords] = {"dog", "cat", "wolf", "bear", "tiger", "cheetah", "elephant"};
+    int errorCount = 0;
+    bool gameWon = false;
+    bool maxErrorsReached = false;
+    bool guessIsCorrect;
 
-    printCurrentBoard(currentBoard, size);
+    while (!gameWon && !maxErrorsReached) {
 
-    printf("Size of char: ");
+        puts("exit 7");
+        getLetterGuess(&playerGuess);
+        puts("exit 8");
 
-    return EXIT_SUCCESS;
+        guessIsCorrect =
+            checkGuess(playerGuess, secretWord, *currentBoard, secretWordSize, gameWon);
+        if (!guessIsCorrect) {
+            errorCount++;
+        }
+
+        printf("%s\n", HANGMANPICS[errorCount]);
+        puts("=====================");
+        puts("=== CURRENT BOARD ===");
+        puts("=====================");
+        printf("%s\n", *currentBoard);
+
+        if (errorCount >= MAX_ERRORS) {
+            maxErrorsReached = true;
+        }
+    }
+
+    return 0;
 }
